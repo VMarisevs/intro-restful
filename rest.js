122d349d0c75453c28710e18599375fb28526f3b
Vladislavs Marisevs <vmarisevs@gmail.com>
2015-10-08 14:51:38 +0100

    Done
    - Added git ignore file.
    - Remove item using array id number
    - Add item using curl with HTTP POST request

--- rest.js
+++ rest.js
@@ -1,6 +1,9 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 var todos = [
   'Clean dog.'
@@ -14,6 +17,38 @@ app.get('/', function(req, res) {
 
 app.get('/todo', function(req, res) {
     res.json(todos);   
+});
+
+app.get('/todo/:num', function(req, res) {	
+    res.json(todos[req.params.num]);
+});
+
+
+/* Advanced 1 */
+// New route to delete item from todos array using id number (link):
+// http://127.0.0.1:8000/remove/2
+app.get('/remove/:num', function(req, res) {
+	
+	id = req.params.num;
+	
+	// checking if the ellemnt id is correct for index of array
+	if (id > -1) {
+		todos.splice(id, 1);
+	}
+	
+    res.json(todos);
+});
+
+
+/* Advanced 2 */
+// New route to add item using HTTP POST request using curl (command):
+// curl.exe -X POST --data "newTodos=Some new To do" http://127.0.0.1:8000/add
+app.post('/add', function(req, res) {
+	
+	var newTodos = req.body.newTodos;
+	
+	todos.push(newTodos);
+    res.json(todos);
 });
 
 var server = app.listen(8000);
